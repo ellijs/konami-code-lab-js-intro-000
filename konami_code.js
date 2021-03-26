1695e78585c26fc6ac2635594552dc47628a3489
ji sun lee <ellijs@hotmail.com>
2021-03-26 03:24:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -20,7 +20,7 @@ function init() {
       index++;
       
       if (index === codes.length) {
-         alert("Congratulations!")
+        alert("Congratulations!");
         
         index = 0;
       }
